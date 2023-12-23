e1fef83ef2ab68021f595f1ccffd5513eff4501b
Trunal <trunaldudhatra083@gmail.com>
2023-12-23 18:13:13 +0530

    ADD:access-control-allow__23/12_06/13

--- src/routes/Admin/Adminroute.js
+++ src/routes/Admin/Adminroute.js
@@ -2119,6 +2119,13 @@ router.post('/addSetting', Admin_Auth, upload.single('image'), async (req, res) 
 
 //get setting data api
 router.get('/SettingData', Admin_Auth, async (req, res) => {
+
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+    res.setHeader('Access-Control-Allow-Credentials', 'true');
+    res.setHeader('Referrer-Policy', 'strict-origin-when-cross-origin');
+
     const data = await WebConfigModel.find()
     res.status(200).json(data)
 })
