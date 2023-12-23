dc17a410e801e8d8e9884a4b19ea99997b8fc82c
Trunal <trunaldudhatra083@gmail.com>
2023-12-23 17:59:15 +0530

    ADD:access-control-allow__23/12_05/59

--- index.js
+++ index.js
@@ -41,6 +41,7 @@ app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
     res.setHeader('Access-Control-Allow-Credentials', 'true');
+    res.setHeader('Referrer-Policy', 'strict-origin-when-cross-origin');
     next();
 });
 const corsOptions = {
